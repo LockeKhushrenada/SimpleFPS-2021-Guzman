fd0e65df6f0a92dfa3d81bee1ed453b437577705
LockeKhushrenada <24321637+LockeKhushrenada@users.noreply.github.com>
2021-10-14 04:52:29 -0500

    Camera Version 1

--- SimpleFPS-2021-Guzman/Assets/CameraControl.cs
+++ SimpleFPS-2021-Guzman/Assets/CameraControl.cs
@@ -18,10 +18,24 @@ public class CameraControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float x = Input.GetAxis("Mouse X") * rotSpeed * Time.deltaTime;
-        float y = Input.GetAxis("Mouse Y") * rotSpeed * Time.deltaTime * -1;
+        float x = Input.GetAxis("Mouse X");
+        float y = Input.GetAxis("Mouse Y");
+        //y = Mathf.Clamp(y * rotSpeed * Time.deltaTime * -1, -90f, 90f);
         transform.Rotate(new Vector3(0, x * rotSpeed * Time.deltaTime, 0));
-        LookUpDown.Rotate(new Vector3(y * rotSpeed * Time.deltaTime * -1, 0, 0));
-        y = Mathf.Clamp(LookUpDown.eulerAngles.y, -90f, 90f);
+        if (LookUpDown.localEulerAngles.y >= -80f && LookUpDown.localEulerAngles.y <= 80f)
+        {
+            LookUpDown.Rotate(new Vector3(y * rotSpeed * Time.deltaTime * -1, 0, 0));
+        }
+        else
+        {
+            if(LookUpDown.localEulerAngles.y * -1 <= -80f)
+            {
+                LookUpDown.Rotate(new Vector3(-1,0,0));
+            }
+            if (LookUpDown.localEulerAngles.y * -1 >= 80f)
+            {
+                LookUpDown.Rotate(-1,0,0);
+            }
+        }
     }
 }
